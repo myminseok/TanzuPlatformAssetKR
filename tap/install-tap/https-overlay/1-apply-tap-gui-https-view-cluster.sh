15aa7d4bec6a4409a9afbb342d2accbf2c04c8d5
myminseok <myminseok@gmail.com>
2022-12-05 17:13:03 +0900

    comment update

--- tap/install-tap/https-overlay/1-apply-tap-gui-https-view-cluster.sh
+++ tap/install-tap/https-overlay/1-apply-tap-gui-https-view-cluster.sh
@@ -16,4 +16,4 @@ if [[ "x$TAP_GUI_CERT" == "x" ]]; then
   exit 1
 fi
 echo "$TAP_GUI_CERT" > /tmp/tap-gui-cert.txt
-cat /tmp/tap-gui-cert.txt
+echo "certificate 'tap-gui-cert' is saved to /tmp/tap-gui-cert.txt "
