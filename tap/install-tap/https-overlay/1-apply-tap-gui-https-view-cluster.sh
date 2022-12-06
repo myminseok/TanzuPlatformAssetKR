31aefbdeea75edeb3c3ce2dcc490f17b90c6d8f9
myminseok <myminseok@gmail.com>
2022-12-06 13:55:17 +0900

    update comment

--- tap/install-tap/https-overlay/1-apply-tap-gui-https-view-cluster.sh
+++ tap/install-tap/https-overlay/1-apply-tap-gui-https-view-cluster.sh
@@ -28,6 +28,8 @@ if [[ "x$TAP_GUI_CERT" == "x" ]]; then
   exit 1
 fi
 echo "$TAP_GUI_CERT" > /tmp/tap-gui-cert.txt
+openssl x509 -text -in  /tmp/tap-gui-cert.txt | grep DNS
 echo "certificate 'tap-gui-cert' is saved to /tmp/tap-gui-cert.txt "
+
 
 
