454f878cc730ad74873d2628395b759faad729cc
myminseok <myminseok@gmail.com>
2022-12-08 03:01:30 +0900

    fix iterate cluster

--- tap/install-tap/multi-iterate-cluster/22-prepare-resources.sh
+++ tap/install-tap/multi-iterate-cluster/22-prepare-resources.sh
@@ -5,6 +5,4 @@ load_env_file $SCRIPTDIR/../tap-env
 
 run_script "$SCRIPTDIR/../https-overlay/1-apply-cnrs-default-tls-run-cluster.sh"
 
-run_script "$SCRIPTDIR/../metastore-access/metadata-store-read-client-view-cluster.sh"
-
 run_script "$SCRIPTDIR/../common-scripts/tap-gui-viewer-service-account-rbac.sh"
