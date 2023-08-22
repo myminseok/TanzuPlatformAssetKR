dfcb1cd325264203b59b3ed7deddb09eb83dd003
myminseok <myminseok@gmail.com>
2023-08-22 10:47:22 +0900

    ingress

--- tap/sample-workload/sample-create-file.sh
+++ tap/sample-workload/sample-create-file.sh
@@ -5,5 +5,5 @@ load_env_file $SCRIPTDIR/tap-env
 DEVELOPER_NAMESPACE=${1:-$DEVELOPER_NAMESPACE}
 
 
-tanzu apps workload delete ${WORKLOAD_NAME} --yes  -n ${DEVELOPER_NAMESPACE}
-tanzu apps workload create -f $SCRIPTDIR/workload-tanzu-java-web-app.yaml --yes   -n ${DEVELOPER_NAMESPACE}
+#tanzu apps workload delete ${WORKLOAD_NAME} --yes  -n ${DEVELOPER_NAMESPACE}
+tanzu apps workload apply -f $SCRIPTDIR/workload-tanzu-java-web-app.yaml --yes   -n ${DEVELOPER_NAMESPACE}
