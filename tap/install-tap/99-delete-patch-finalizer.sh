54f9a1333283fec2b39f15bdbd8d4a3a74de96d0
myminseok <myminseok@gmail.com>
2023-12-26 17:17:54 +0900

    finalizer

---
+++ tap/install-tap/99-delete-patch-finalizer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $SCRIPTDIR/common-scripts/common.sh
+load_env_file $SCRIPTDIR/tap-env
+
+APP_LIST=$(kubectl get app -n tap-install | egrep "stack|buildpack|builder|amr-observer"  | awk '{print $1}')
+echo $APP_LIST
+
+confirm_target_k8s "Are you sure to DELETE?: "
+
+for app in $APP_LIST;
+do
+  echo $app
+  set +x
+  kubectl patch app/$app -n tap-install -p '{"metadata":{"finalizers":null}}' --type=merge
+done
