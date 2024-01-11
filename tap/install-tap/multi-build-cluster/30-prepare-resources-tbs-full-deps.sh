fbef134a97533e972198e36ead6e1ee5a3391a15
myminseok <myminseok@gmail.com>
2024-01-11 16:38:09 +0900

    delete

--- tap/install-tap/multi-build-cluster/30-prepare-resources-tbs-full-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $SCRIPTDIR/../common-scripts/common.sh
-load_env_file $SCRIPTDIR/../tap-env
-
-$SCRIPTDIR/../30-prepare-resources-tbs-full-deps.sh $@
--- tap/install-tap/multi-build-cluster/31-install-tbs-full-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $SCRIPTDIR/../common-scripts/common.sh
-load_env_file $SCRIPTDIR/../tap-env
-
-$SCRIPTDIR/../31-install-tbs-full-deps.sh $@
