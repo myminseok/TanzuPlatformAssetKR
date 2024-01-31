18440884143d794556906cf925faaec9de6cb2e3
myminseok <myminseok@gmail.com>
2024-01-31 10:29:50 +0900

    update

--- tap/sample-workload/tanzu-java-web-app-workload/sample-create-file.sh
+++ tap/sample-workload/tanzu-java-web-app-workload/sample-create-file.sh
@@ -3,3 +3,6 @@ DEVELOPER_NAMESPACE=${1:-my-space}
 
 tanzu apps workload delete tanzu-java-web-app --yes -n ${DEVELOPER_NAMESPACE}
 tanzu apps workload apply -f ./workload-tanzu-java-web-app.yaml --yes   -n ${DEVELOPER_NAMESPACE}
+
+
+## kubectl get cm -n my-space tanzu-java-web-app-deliverable -o jsonpath='{.data.deliverable}' > ./tanzu-java-web-app-deliverable.yml
