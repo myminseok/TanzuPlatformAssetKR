326fe4c5cf781db96046137cbd1e157e3a2d503b
myminseok <myminseok@gmail.com>
2024-02-21 14:24:50 +0900

    remove sample apps

--- tap/sample-workload/node-http/deploy.sh
+++
@@ -1,8 +0,0 @@
-
-#tanzu apps workload delete node-http -n my-space --yes
-
-tanzu apps workload apply -f config/workload.yaml -n my-space --yes
-
-watch tanzu apps workload get node-http --namespace my-space 
-
-## kubectl get cm -n my-space node-http-deliverable -o jsonpath='{.data.deliverable}' > ./deliverable.yml
