a1703702b86c9b08b9593e76265411f276c72aa2
myminseok <myminseok@gmail.com>
2025-01-02 14:28:30 +0900

    update script

--- tap/install-tap/99-delete-app-finalizer.sh
+++ tap/install-tap/99-delete-app-finalizer.sh
@@ -3,8 +3,8 @@ SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $SCRIPTDIR/common-scripts/common.sh
 load_env_file $SCRIPTDIR/tap-env
 
-## APP_LIST=$(kubectl get app -n tap-install  | grep -v "NAME" | awk '{print $1}')
-APP_LIST=$(kubectl get app -n tap-install | egrep "stack|buildpack|builder|amr-observer"  | awk '{print $1}')
+APP_LIST=$(kubectl get app -n tap-install  | grep -v "NAME" | awk '{print $1}')
+#APP_LIST=$(kubectl get app -n tap-install | egrep "stack|buildpack|builder|amr-observer"  | awk '{print $1}')
 echo $APP_LIST
 
 
@@ -16,5 +16,5 @@ do
   echo $app
   set +x
   kubectl patch app/$app -n tap-install -p '{"metadata":{"finalizers":null}}' --type=merge
-  # kubectl patch app/$app -n tap-install -p '[{"path":"/metadata/finalizers","op": "remove"}]'  --type=json
+#:  kubectl patch app/$app -n tap-install -p '[{"path":"/metadata/finalizers","op": "remove"}]'  --type=json
 done
